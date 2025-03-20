2b48d9344e1be28d69b3da9926f144a91014038e
Brien Colwell <xcolwell@gmail.com>
2025-03-19 17:41:23 -0700

    build start continuous build

---
+++ all/build-all.sh
@@ -0,0 +1,12 @@
+
+# pull all repos
+# create version branch on all repos
+# save warp.json file with warp version
+# edit android gradle local.properties and xcodeproject files with version values
+# edit fdroiddata manifest
+# push branches
+
+# upload android build
+# upload xcode archive build
+
+
