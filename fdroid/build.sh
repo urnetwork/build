632496f98785d8faf72c2027de9c73de36b307ce
Brien Colwell <xcolwell@gmail.com>
2025-04-07 12:03:27 -0700

    build update

--- fdroid/build.sh
+++ fdroid/build.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 (sudo apt-get update &&
 	sudo apt-get install -y gcc libc-dev make &&
@@ -23,8 +23,13 @@ export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/28.0.13004108
 		-keyalg RSA \
 		-validity 14000)
 
-if [[ ! "$(go version)" =~ "^go version go1.24.2|^$" ]]; then
-	echo "system go must either be absent or version go1.24.2"
+go_version=`go version 2> /dev/null`
+if [ "$go_version" == "" ]; then
+	echo "go check: go will use /usr/local/go ($(/usr/local/go/bin/go version))"
+elif [[ "$go_version" =~ "go version go1.24.2" ]]; then
+	echo "go check: go will use system go ($go_version)"
+else
+	echo "go check: system go must either be 1.24.2 or not installed"
 	exit 1
 fi
 
