fe4361759ba1f1426ba3904f8511ee4519f9aed0
Brien Colwell <xcolwell@gmail.com>
2025-06-15 07:09:14 -0700

    build: use go 1.24.4

--- all/run.sh
+++ all/run.sh
@@ -86,8 +86,8 @@ ANDROID_NDK_VERSION=28.0.13004108
 sdkmanager "ndk;$ANDROID_NDK_VERSION"
 error_trap 'android ndk'
 export ANDROID_NDK_HOME="$ANDROID_HOME/ndk/$ANDROID_NDK_VERSION"
-if [[ ! `go version` =~ 'go version go1.24.3' ]]; then
-    builder_message 'go 1.24.3 required'
+if [[ ! `go version` =~ 'go version go1.24.4' ]]; then
+    builder_message 'go 1.24.4 required'
     exit 1
 fi
 if [[ ! `java -version 2>&1` =~ 'openjdk version "21.0.7"' ]]; then
