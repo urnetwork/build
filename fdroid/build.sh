ae7e4faabb28064b2615a24b82ffb6919ca23b4a
Brien Colwell <xcolwell@gmail.com>
2025-10-13 00:48:03 -0500

    build: update to go 1.25.2

--- fdroid/build.sh
+++ fdroid/build.sh
@@ -14,7 +14,7 @@ error_trap () {
 	sudo apt-get update &&
 	sudo apt-get install -y -t trixie openjdk-21-jdk-headless &&
 	sudo update-alternatives --auto java &&
-	curl -L https://go.dev/dl/go1.25.0.linux-amd64.tar.gz | sudo tar -xz -C /usr/local/)
+	curl -L https://go.dev/dl/go1.25.2.linux-amd64.tar.gz | sudo tar -xz -C /usr/local/)
 error_trap 'root init'
 
 export ANDROID_HOME=/opt/android-sdk
@@ -38,10 +38,10 @@ error_trap 'android debug key init'
 go_version=`go version 2> /dev/null`
 if [ "$go_version" == "" ]; then
 	echo "go check: go will use /usr/local/go ($(/usr/local/go/bin/go version))"
-elif [[ "$go_version" =~ "go version go1.25.0" ]]; then
+elif [[ "$go_version" =~ "go version go1.25.2" ]]; then
 	echo "go check: go will use system go ($go_version)"
 else
-	echo "go check: system go must either be 1.25.0 or not installed"
+	echo "go check: system go must either be 1.25.2 or not installed"
 	exit 1
 fi
 java_version=`java -version 2>&1`
