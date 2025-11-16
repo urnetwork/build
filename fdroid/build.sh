e35e2684e6a14a11cde1eca147b0d827bef30893
Brien Colwell <xcolwell@gmail.com>
2025-11-16 02:33:38 -0600

    fdroid: update java version

--- fdroid/build.sh
+++ fdroid/build.sh
@@ -46,9 +46,12 @@ else
 fi
 java_version=`java -version 2>&1`
 if [[ ! "$java_version" =~ 'openjdk version "21.0.8"' ]]; then
-    echo "java check: 21.0.8 required ($java_version)"
-    exit 1
+	if [[ ! "$java_version" =~ 'openjdk version "21.0.9"' ]]; then
+	    echo "java check: 21.0.8 or 21.0.9 required ($java_version)"
+	    exit 1
+	fi
 fi
+
 
 export WARP_HOME=/urnetwork
 export BRINGYOUR_HOME=/urnetwork/build
